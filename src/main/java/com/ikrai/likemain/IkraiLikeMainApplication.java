47e2647b4f0681f2da409ea358cea8f5f9932b49
ikrai11 <hhnawws@gmail.com>
2026-02-05 15:37:07 +0800

    初始化项目

---
+++ src/main/java/com/ikrai/likemain/IkraiLikeMainApplication.java
@@ -0,0 +1,13 @@
+package com.ikrai.likemain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkraiLikeMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IkraiLikeMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ikrai/likemain/IkraiLikeMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikrai.likemain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkraiLikeMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
